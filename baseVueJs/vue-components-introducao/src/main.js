300aca35ec8151e5de2066f6d4d1804e1c117ee8
Vagner Cazarotto <vagnercazarotto@gmail.com>
2020-05-21 11:33:47 +0200

    data propriety as a function

--- baseVueJs/vue-components-introducao/src/main.js
+++ baseVueJs/vue-components-introducao/src/main.js
@@ -1,10 +1,30 @@
 import Vue from 'vue'
 
+// don't return a shared object to a component
+/*const data = {
+  titulo: 'curso de vue JS',
+  contador: 0
+}
+*/
+
 Vue.component('pjn-component', { 
   data: function() {
-    return { titulo: 'curso de vue JS' }
+    return {
+      titulo: 'curso de vue JS',
+      contador: 0
+    }
   },
-  template: '<h1>{{ titulo }} </h1>'
+  methods: {
+    incrementar: function(){
+      this.contador++
+    }
+  },
+  template:`
+    <div>
+      <h1>{{ titulo }} </h1>
+      <button @click="incrementar">Clicado {{ contador }} vezes!</button>
+    </div>
+  `  
 })
 
 new Vue({
