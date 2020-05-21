fa2e88ff673e22c9626d856c828167cf5251c562
Vagner Cazarotto <vagnercazarotto@gmail.com>
2020-05-21 11:26:56 +0200

    Introduction to components

--- baseVueJs/vue-components-introducao/src/main.js
+++ baseVueJs/vue-components-introducao/src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 
+Vue.component('pjn-component', { 
+  data: function() {
+    return { titulo: 'curso de vue JS' }
+  },
+  template: '<h1>{{ titulo }} </h1>'
+})
+
 new Vue({
- el: '#app',
- template: '<h1>Vue components</h1>'
+ el: '#app'
 })
