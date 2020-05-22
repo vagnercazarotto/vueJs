eb2ac37940181d1b65162b594ea300432384c7e3
Vagner Cazarotto <vagnercazarotto@gmail.com>
2020-05-22 10:48:37 +0200

    code -- pre slots

--- baseVueJs/vue-components-avancado/src/App.vue
+++ baseVueJs/vue-components-avancado/src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <h1>Vue JS</h1>
+
+    <Post titulo="Component no Vue"/>
+    <Post titulo="Passando props via props"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Post from './components/Post.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Post
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
