d1a4ec9d684f5b7623501b5018b5c9defb26a9a7
Vagner Cazarotto <vagnercazarotto@gmail.com>
2020-05-25 11:06:27 +0200

    fixed project name + cleaned app

--- baseVueJs/vue-rotas/README.md
+++ baseVueJs/vue-rotas/README.md
@@ -1,4 +1,4 @@
-# vue-router
+# vue-rotas
 
 ## Project setup
 ```
---
+++ baseVueJs/vue-rotas/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+
+    <div class="jumbotron">
+      <h1 class="display-4">Vue Router!</h1>
+      <p class="lead"> Adicionando rotas a single page Application</p>
+    </div>
+
+    <div class="container">
+      <h3 class="font-weight-light">Contatos</h3>
+    </div>
+
+  </div>
+</template>
+
