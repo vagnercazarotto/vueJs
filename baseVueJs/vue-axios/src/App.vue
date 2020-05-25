6cb70b1b64831a34d7200c8646e2843691cd4b3f
Vagner Cazarotto <vagnercazarotto@gmail.com>
2020-05-25 21:13:08 +0200

    cleaned project to use Axios

--- baseVueJs/vue-axios/src/App.vue
+++ baseVueJs/vue-axios/src/App.vue
@@ -1,28 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4"> Requisiçoes HTTP </h1>
+        <p class="lead">Usando biblioteca Axios para fazer chamadas Ajax a uma API REST!</p>
+      </div> 
+    </div>
+
+
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
