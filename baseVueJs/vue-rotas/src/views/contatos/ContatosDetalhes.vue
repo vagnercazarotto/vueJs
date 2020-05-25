95657361b6e73ee478746ab1449c5ffe16b69c88
Vagner Cazarotto <vagnercazarotto@gmail.com>
2020-05-25 14:13:24 +0200

    catching params

--- baseVueJs/vue-rotas/src/views/contatos/ContatosDetalhes.vue
+++ baseVueJs/vue-rotas/src/views/contatos/ContatosDetalhes.vue
@@ -1,3 +1,11 @@
 <template>
-    <h3 class="font-weight-light">Detalhes sobre o contato</h3>
+    <h3 class="font-weight-light">Detalhes sobre o contato com o id: {{ this.$route.params.id }} </h3>
 </template>
+
+<script>
+export default {
+    created() {
+        console.log('parametos da rota', this.$route.params)
+    }
+}
+</script>
