314a383017b6596b815c2413636c5d1e34799262
Vagner Cazarotto <vagnercazarotto@gmail.com>
2020-05-25 09:58:56 +0200

    clean - vue--animations

---
+++ baseVueJs/vue-animation/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4"> Animacoes </h1>
+        <p class="lead"> treinando animacoes </p>
+      </div>
+    </div>
+
+    <div class="container"></div>
+
+  </div>
+</template>
+
+
+
+
