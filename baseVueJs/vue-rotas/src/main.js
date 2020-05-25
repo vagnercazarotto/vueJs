85a6944e393ffba428a3221e2655cb0c9c6eabbe
Vagner Cazarotto <vagnercazarotto@gmail.com>
2020-05-25 11:39:04 +0200

    vue router in history mode

--- baseVueJs/vue-rotas/src/main.js
+++ baseVueJs/vue-rotas/src/main.js
@@ -10,6 +10,7 @@ Vue.config.productionTip = false
 Vue.use(VueRouter)
 
 const router = new VueRouter({
+  mode: 'history',
   routes: [
     { path: '/contatos', component: Contatos }, // meus-contatos.com/contatos
     { path: '/', component: Home }
