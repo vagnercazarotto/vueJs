c671b95973a0171834380ab0e309746517911cf6
Vagner Cazarotto <vagnercazarotto@gmail.com>
2020-05-25 11:14:07 +0200

    callingr router plugin

--- baseVueJs/vue-rotas/src/main.js
+++ baseVueJs/vue-rotas/src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
 
+// tell Vue what native plug in we should use
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: []
+})
+
 new Vue({
+  router: router,
   render: h => h(App),
 }).$mount('#app')
